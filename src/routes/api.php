fe63dc047a67c1db0ef5117680d6324d9d39b014
vitalii boiko <vukelan@gmail.com>
2024-12-20 23:16:36 +0200

    add route api

---
+++ src/routes/api.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+// Route::middleware("auth:sanctum")->get("/user", function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get("/user", function (Request $request) {
+    return $request->user();
+})->middleware('auth.basic.once');
+
+Route::get("/test", function () {
+    return new JsonResponse("Hello, API");
+});
+
+Route::get("/token", function () {
+    return new JsonResponse([
+      "sucess"=>true,
+      "token"=>
+    ]);
+});
+
+Route::get("/test/error", function () {
+    throw new BadRequestHttpException("Test Error Trace");
+});
