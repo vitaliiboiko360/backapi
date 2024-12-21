cb74024908821a712ff7e1e14c2c4ebbd551dca7
vitalii boiko <vukelan@gmail.com>
2024-12-21 23:41:01 +0200

    add api token model

---
+++ src/app/Models/ApiToken.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Prunable;
+use Illuminate\Database\Eloquent\Model;
+
+class ApiToken extends Model
+{
+  use Prunable;
+  /**
+   * The table associated with the model.
+   *
+   * @var string
+   */
+  protected $table = 'api_tokens';
+  /**
+   * The primary key associated with the table.
+   *
+   * @var string
+   */
+  protected $primaryKey = 'token_id';
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array<int, string>
+   */
+  protected $fillable = ["token"];
+
+  const CREATED_AT = 'created_timestamp';
+  const UPDATED_AT = 'updated_timestamp';
+  const EXPIRE_AFTER_40_MINUTES = 40;
+
+  /**
+   * Get the prunable model query.
+   */
+  public function prunable(): Builder
+  {
+    return static::where(self::CREATED_AT, '<=', now()->addMinutes(self::EXPIRE_AFTER_40_MINUTES));
+  }
+}
