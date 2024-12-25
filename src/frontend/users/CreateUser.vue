71f788fc23ddbae5098259c56024a04d9dbc1129
vitalii boiko <vukelan@gmail.com>
2024-12-25 23:30:44 +0200

    formatting output form response

--- src/frontend/users/CreateUser.vue
+++ src/frontend/users/CreateUser.vue
@@ -1,34 +1,56 @@
 <script setup>
-const outputResponse = (data) => {
-    console.log(JSON.stringify(data));
-};
+import { ref, watch } from "vue";
+const userFormRef = ref();
+const outputResponseRef = ref();
+const outputResponse = ref();
 
-const submit = async () => {
-    let formData = new FormData(userForm);
+const submit = async (userFormRef) => {
+    let formData = new FormData(userFormRef);
     try {
         const response = await fetch("/api/users", {
             method: "POST",
             body: formData,
         });
-        outputResponse(await response.json());
+
+        outputResponse.value = await response.json();
     } catch (e) {
         console.error(e);
     }
 };
+
+watch([outputResponse, outputResponseRef], () => {
+    if (!(outputResponseRef.value && outputResponse.value)) return;
+    console.log(outputResponse.value);
+    const { message, fails = "", user_id = "" } = outputResponse.value;
+    outputResponseRef.value.textContent = `${message}${
+        fails
+            ? "\r\n failed: \r\n\r\n" +
+              Object.entries(fails)
+                  .map((element) => `${element[0]}:\r\n${element[1]}`)
+                  .join("\r\n\r\n")
+            : fails
+    }${user_id ? "\r\n user ID: " + user_id : user_id}`;
+});
 </script>
 
 <template>
     <div :class="$style.formContainerDiv">
-        <form :id="userForm" @submit.prevent="submit">
+        <form :ref="(el) => (userFormRef = el)" @submit.prevent="() => submit(userFormRef)">
             <h3 :class="$style.formTitleH3">Add New User</h3>
+            <p
+                :ref="(el) => (outputResponseRef = el)"
+                :class="outputResponse == undefined ? '' : outputResponse.success ? $style.success : $style.failure"
+            ></p>
             <p>
                 Token: you should get a proper token at
-                <span>&nbsp;<a href="/api/token" target="_blank" rel="noreferrer noopener">/api/token</a></span>
+                <span
+                    >&nbsp;<a :href="`/api/token`" :target="`_blank`" :rel="`noreferrer noopener`">/api/token</a></span
+                >
             </p>
-            <input v-model="token" placeholder="Enter token (128 characters length)" />
+            <input v-model="token" :name="`token`" placeholder="Enter token (128 characters length)" />
             <p></p>
             <p>Name: Username should contain 2-60 characters.</p>
-            <input v-model="name" placeholder="name" />
+            <input v-model="name" placeholder="name" :name="`name`" />
             <p>Phone: User phone number. Number should start with code of Ukraine +380.</p>
             <input v-model="phone" placeholder="+380XXXXXXXXX" />
             <p>Email: User email, must be a valid email according to RFC2822.</p>
@@ -45,16 +67,20 @@ const submit = async () => {
             <input :type="`file`" :accept="`image/*,.jpeg,.jpg`" />
 
             <div :class="$style.buttonContainerDiv">
-                <input type="submit" value="Submit" />
+                <input :type="`submit`" :value="Submit" />
             </div>
         </form>
     </div>
 </template>
 
 <style module>
+a {
+    color: blue;
+}
 input {
     padding: 0.3rem;
     border: 1px grey solid;
+    width: 100%;
 }
 .formContainerDiv {
     margin-bottom: 2rem;
@@ -65,5 +91,23 @@ input {
 }
 .buttonContainerDiv {
     margin: 0.3rem;
+}
+.success {
+    white-space: pre-line;
+    margin: 0.5rem;
+    padding: 0.5rem;
+    border: 2px green solid;
+    color: darkslategrey;
+    font-size: 1.5rem;
+    border-radius: 1rem;
+}
+.failure {
+    white-space: pre-line;
+    margin: 0.5rem;
+    padding: 0.5rem;
+    border: 3px red solid;
+    border-radius: 1rem;
+    color: darkred;
+    font-size: 1.3rem;
 }
 </style>
