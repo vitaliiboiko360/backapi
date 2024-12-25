7d6c6abf0e8b241bde527e31f1d2bb08f4a01e30
vitalii boiko <vukelan@gmail.com>
2024-12-25 17:04:16 +0200

    add vue frontend

---
+++ src/frontend/App.vue
@@ -0,0 +1,23 @@
+<script setup></script>
+
+<template>
+    <div :class="$style.outerDiv">
+        <div :class="$style.innerDiv">
+            <h2>Users</h2>
+            <p>test page</p>
+        </div>
+    </div>
+</template>
+
+<style module>
+.outerDiv {
+    display: flex;
+    flex-direction: column;
+}
+
+.innerDiv {
+    min-width: 30%;
+    max-width: 30%;
+    align-self: center;
+}
+</style>
