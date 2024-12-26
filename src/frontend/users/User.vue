e30a88bbd6882ff3a80ea50b8d650062ee1d2344
vitalii boiko <vukelan@gmail.com>
2024-12-26 20:11:29 +0200

    add pagination to frontend

--- src/frontend/users/User.vue
+++ src/frontend/users/User.vue
@@ -1,11 +1,36 @@
-<script setup></script>
+<script setup>
+import { defineProps } from "vue";
+const { name = "", phone = "", email = "", photo = "" } = defineProps(["name", "phone", "email", "photo"]);
+</script>
 
 <template>
-    <div>
-        <p>Name</p>
-        <p>Phone</p>
-        <p>Email</p>
-        <img :alt="`No photo yet`"></img>
-        <br/>
+    <div :class="$style.divOuter">
+        <div><img :src="photo" :alt="`No photo yet`" /></div>
+        <div>
+            <div :class="$style.divUserCard">
+                <p>Name: {{ name }}</p>
+                <p>Phone: {{ phone }}</p>
+                <p>Email: {{ email }}</p>
+            </div>
+        </div>
     </div>
 </template>
+
+<style module>
+img {
+    min-width: 5rem;
+    min-height: 5rem;
+    max-width: 8rem;
+    max-height: 8rem;
+}
+p {
+    margin: 0 0.5rem 0.5rem;
+}
+.divOuter {
+    display: flex;
+}
+.divUserCard {
+    display: flex;
+    flex-direction: column;
+}
+</style>
--- src/frontend/users/Users.vue
+++ src/frontend/users/Users.vue
@@ -1,13 +1,69 @@
 <script setup>
+import { onMounted, ref, watch } from "vue";
 import User from "./User.vue";
 import CreateUser from "./CreateUser.vue";
+const outputResponse = ref();
+const currentPage = ref(1);
+const DEFAULT_EMPTY_NUMBER_OF_USERS = 5;
+const DEFAULT_NUMBER_OF_USERS_TO_SHOW = 6;
+
+const getUsers = async () => {
+    try {
+        const response = await fetch(`/api/users?count=${DEFAULT_NUMBER_OF_USERS_TO_SHOW}&page=${currentPage.value}`);
+        outputResponse.value = await response.json();
+        console.log(JSON.stringify(outputResponse.value));
+    } catch (e) {
+        /** do not clutter console */
+    }
+};
+
+onMounted(getUsers);
+watch(currentPage, getUsers);
 </script>
 
 <template>
     <div>
-        <User v-for="user in 5" :key="user" />
+        <User
+            v-for="(user, index) in outputResponse?.users ?? DEFAULT_EMPTY_NUMBER_OF_USERS"
+            :key="index"
+            :name="user.name"
+            :email="user.email"
+            :phone="user.phone"
+            :photo="user.photo"
+        />
+    </div>
+    <div>
+        <button
+            :disabled="outputResponse?.links?.prev_url == null ? true : false"
+            @click="
+                () => {
+                    console.log(outputResponse.links.prev_url);
+                    currentPage = currentPage - 1;
+                }
+            "
+        >
+            &lt&lt&nbspPrev&nbspPage</button
+        >&nbsp
+        <button
+            :disabled="outputResponse?.links?.next_url == null ? true : false"
+            @click="
+                () => {
+                    console.log(outputResponse.links.next_url);
+                    currentPage = Math.min(currentPage + 1, outputResponse.total_pages);
+                }
+            "
+        >
+            Next&nbspPage&nbsp&gt&gt
+        </button>
     </div>
     <div>
         <CreateUser />
     </div>
 </template>
+
+<style module>
+button {
+    font-size: 2rem;
+    margin: 1rem 0;
+}
+</style>
