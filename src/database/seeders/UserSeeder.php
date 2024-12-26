9739f256e7836e56953b8001ffcb215f0324c65f
vitalii boiko <vukelan@gmail.com>
2024-12-26 17:32:49 +0200

    add seeder

---
+++ src/database/seeders/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+  use WithoutModelEvents;
+  /**
+   * Run user seeder
+   *
+   * @return void
+   */
+  public function run()
+  {
+    User::factory()
+      ->count(50)
+      ->create();
+  }
+}
