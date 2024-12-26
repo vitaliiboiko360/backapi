646030627fb04cdc7ac236a7489c8cddf852e669
vitalii boiko <vukelan@gmail.com>
2024-12-26 17:34:48 +0200

    add constants and utils

---
+++ src/app/Constants.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+class Constants
+{
+  const UKRAINE_MOBILE_CODES = ["66", "99", "95", "50", "68", "96", "86", "63", "93"];
+  const PHOTO_MAX_FILESIZE = 5 * 1024;
+  const PHOTO_MIN_DIM_HEIGHT = 70;
+  const PHOTO_MIN_DIM_WIDTH = 70;
+}
